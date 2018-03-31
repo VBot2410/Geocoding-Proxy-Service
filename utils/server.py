8736f741fedb545320a3c20f9c1cf0e4afc844c8
VBot2410 <vaibhav24.vb@gmail.com>
2018-03-31 03:09:33 -0400

    Task:Server, Implemented server which uses hostname and port to start a server and keeps running forever

--- utils/server.py
+++ utils/server.py
@@ -1,3 +1,22 @@
 """
 server script that starts the server using given host and port & keeps it running forever
 """
+# imports
+from http.server import HTTPServer
+from utils.request_handler import RestHTTPRequestHandler
+
+
+def run_server(host, port):
+    """
+    Use host and port to start the server and keep it running.
+
+    Parameters:
+        host: A string specifying the hostname.
+        port: A string specifying the port.
+
+    Return:
+        None
+    """
+    httpd = HTTPServer((host, port), RestHTTPRequestHandler)
+    print('Running server at host = {} and port = {}'.format(host, port))
+    httpd.serve_forever()
